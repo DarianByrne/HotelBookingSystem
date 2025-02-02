4a1bd588ce94fefc03d8b3a91a9ae36fe88498df
DarianByrne <173167730+DarianByrne@users.noreply.github.com>
2025-02-02 16:48:06 +0000

    Added BrowseRooms and ViewAvailability

--- src/ie/hotelbooking/view/BrowseRooms.java
+++ src/ie/hotelbooking/view/BrowseRooms.java
@@ -1,4 +1,57 @@
 package ie.hotelbooking.view;
 
-public class BrowseRooms {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+
+public class BrowseRooms extends JFrame {
+	private final JPanel panel1;
+	private final JLabel label1;
+	private final JComboBox<String> roomTypeComboBox;
+	private final JLabel label2;
+	private final JButton viewAvailabilityButton;
+
+	public BrowseRooms() {
+		panel1 = new JPanel();
+		setContentPane(panel1);
+		setTitle("Browse Rooms");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(800, 600);
+		setLocationRelativeTo(null);
+//		setLayout(new BoxLayout(panel1, BoxLayout.Y_AXIS));
+
+		label1 = new JLabel("Select a room type");
+		panel1.add(label1);
+
+		String[] roomTypes = {"Single", "Double", "King"};
+		roomTypeComboBox = new JComboBox<>(roomTypes);
+		roomTypeComboBox.addItemListener(this::itemStateChanged);
+		panel1.add(roomTypeComboBox);
+
+		label2 = new JLabel("Room Type");
+		panel1.add(label2);
+
+		viewAvailabilityButton = new JButton("View Availability");
+		viewAvailabilityButton.addActionListener(this::actionPerformed);
+		panel1.add(viewAvailabilityButton);
+
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new BrowseRooms();
+	}
+
+	public void itemStateChanged(ItemEvent e) {
+		if (e.getSource() == roomTypeComboBox) {
+			label2.setText(roomTypeComboBox.getSelectedItem() + " selected");
+		}
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == viewAvailabilityButton) {
+			new ViewAvailability(roomTypeComboBox.getSelectedItem() + " size bedroom");
+		}
+	}
 }
--- src/ie/hotelbooking/view/ViewAvailability.java
+++ src/ie/hotelbooking/view/ViewAvailability.java
@@ -1,4 +1,49 @@
 package ie.hotelbooking.view;
 
-public class ViewAvailability {
+import com.github.lgooddatepicker.components.DatePicker;
+import com.github.lgooddatepicker.zinternaltools.DateChangeEvent;
+
+import javax.swing.*;
+
+public class ViewAvailability extends JFrame {
+	private final JPanel panel1;
+	private final JLabel label1;
+	private final DatePicker datePicker;
+	private final JLabel label2;
+
+	public ViewAvailability(String type) {
+		panel1 = new JPanel();
+		setContentPane(panel1);
+		setTitle("View Availability");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(800, 600);
+		setLocationRelativeTo(null);
+//		setLayout(new BoxLayout(panel1, BoxLayout.Y_AXIS));
+
+		label1 = new JLabel("View " + type + " Availability");
+		panel1.add(label1);
+
+		datePicker = new DatePicker();
+		datePicker.addDateChangeListener(this::dateChanged);
+		panel1.add(datePicker);
+
+		label2 = new JLabel("Select A Date");
+		panel1.add(label2);
+
+		setVisible(true);
+	}
+
+	public ViewAvailability() {
+		this("");
+	}
+
+	public static void main(String[] args) {
+		new ViewAvailability();
+	}
+
+	public void dateChanged(DateChangeEvent e) {
+		if (e.getSource() == datePicker) {
+			label2.setText(datePicker.getDate() + " selected");
+		}
+	}
 }
