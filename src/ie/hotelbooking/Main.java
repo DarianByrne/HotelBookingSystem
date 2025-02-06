a75e0c2ebbfe9da85e09261e549fc2d07cc7d6ba
kylepunta <kylepurcell1998@gmail.com>
2025-02-06 09:21:26 +0000

    Removed code in main method

--- src/ie/hotelbooking/Main.java
+++ src/ie/hotelbooking/Main.java
@@ -10,31 +10,6 @@ import java.sql.Time;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
 
-		String name = "Mick Phelan";
-		String phoneNumber = "086-1602798";
-		String email = "mick.phelan@gmail.com";
-		Date dateOfBirth = Date.valueOf("2000-10-10");
-		String address = "Cork";
-
-		String nameToUpdate = "Donald Trump";
-
-		CustomerController customerDAO = new CustomerController();
-		Customer customerOne = new Customer(name, phoneNumber, email, dateOfBirth, address);
-
-		customerDAO.displayCustomers();
-
-		Payment paymentOne = new Payment();
-		PaymentController paymentDAO = new PaymentController();
-		paymentOne.setCustomer(customerOne);
-		paymentOne.setCardHolderName("Donald Trump");
-		paymentOne.setCardNumber(0000);
-		paymentOne.setCardExpiryDate(Date.valueOf("2000-10-10"));
-		paymentOne.setCardCVV(987);
-		paymentOne.setTime(Time.from("12:00"));
-		paymentOne.setAmount(200);
-
-		paymentDAO.displayPayments();
 	}
 }
