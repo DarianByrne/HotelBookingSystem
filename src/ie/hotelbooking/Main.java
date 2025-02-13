c53b0429c68ea14693d2e182a8a24e90585fe973
DarianByrne <173167730+DarianByrne@users.noreply.github.com>
2025-02-13 10:23:12 +0000

    Removed old code

--- src/ie/hotelbooking/Main.java
+++ src/ie/hotelbooking/Main.java
@@ -1,16 +1,8 @@
 package ie.hotelbooking;
 
-import ie.hotelbooking.model.booking.Booking;
-import ie.hotelbooking.model.customer.Customer;
-import ie.hotelbooking.model.information.Payment;
 import ie.hotelbooking.view.MainMenu;
 import javax.swing.*;
 import java.awt.*;
-import java.sql.Date;
-import java.sql.Time;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.temporal.ChronoUnit;
 
 public class Main {
 	public static JFrame frame = new JFrame("Hotel Booking System");
@@ -23,44 +15,44 @@ public class Main {
 		frame.setLayout(new FlowLayout(FlowLayout.CENTER));
 		new MainMenu();
 
-		LocalDate today = LocalDate.now();
-		LocalTime time = LocalTime.now().truncatedTo(ChronoUnit.MINUTES);
-
-		Customer customer = new Customer();
-		String name = "Marco Polo";
-		Date dateOfBirth = Date.valueOf("01/01/1990");
-		String address = "Swaziland";
-		String phone = "123456789";
-		String email = "marcopolo@gmail.com";
-		customer.setName(name);
-		customer.setDateOfBirth(dateOfBirth);
-		customer.setAddress(address);
-		customer.setPhoneNumber(phone);
-		customer.setEmail(email);
-
-		Payment payment = new Payment();
-		payment.setCustomer(customer);
-		payment.setCustomerID();
-		int cardNumber = 12345678;
-		int cardCVV = 321;
-		String cardHolderName = "Marco Polo";
-		Date cardExpiryDate = Date.valueOf("01/01/2028");
-		Date paymentDate = Date.valueOf(today);
-		Time paymentTime = Time.valueOf(time);
-		double amount = 200;
-		payment.setCardNumber(cardNumber);
-		payment.setCardCVV(cardCVV);
-		payment.setCardHolderName(cardHolderName);
-		payment.setCardExpiryDate(cardExpiryDate);
-		payment.setCustomerID();
-		payment.setDate(paymentDate);
-		payment.setTime(paymentTime);
-		payment.setAmount(amount);
-
-		Booking booking = new Booking(payment, customer);
-
-		customer.addCustomer();
-		payment.addPayment();
-		booking.addBooking();
+//		LocalDate today = LocalDate.now();
+//		LocalTime time = LocalTime.now().truncatedTo(ChronoUnit.MINUTES);
+//
+//		Customer customer = new Customer();
+//		String name = "Marco Polo";
+//		Date dateOfBirth = Date.valueOf("01/01/1990");
+//		String address = "Swaziland";
+//		String phone = "123456789";
+//		String email = "marcopolo@gmail.com";
+//		customer.setName(name);
+//		customer.setDateOfBirth(dateOfBirth);
+//		customer.setAddress(address);
+//		customer.setPhoneNumber(phone);
+//		customer.setEmail(email);
+//
+//		Payment payment = new Payment();
+//		payment.setCustomer(customer);
+//		payment.setCustomerID();
+//		int cardNumber = 12345678;
+//		int cardCVV = 321;
+//		String cardHolderName = "Marco Polo";
+//		Date cardExpiryDate = Date.valueOf("01/01/2028");
+//		Date paymentDate = Date.valueOf(today);
+//		Time paymentTime = Time.valueOf(time);
+//		double amount = 200;
+//		payment.setCardNumber(cardNumber);
+//		payment.setCardCVV(cardCVV);
+//		payment.setCardHolderName(cardHolderName);
+//		payment.setCardExpiryDate(cardExpiryDate);
+//		payment.setCustomerID();
+//		payment.setDate(paymentDate);
+//		payment.setTime(paymentTime);
+//		payment.setAmount(amount);
+//
+//		Booking booking = new Booking(payment, customer);
+//
+//		customer.addCustomer();
+//		payment.addPayment();
+//		booking.addBooking();
 	}
 }
