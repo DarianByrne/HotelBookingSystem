428264da7bbc111007e1c1cbde302b716f18583e
DarianByrne <173167730+DarianByrne@users.noreply.github.com>
2025-03-04 10:50:46 +0000

    Updated Screens

--- src/ie/hotelbooking/view/PurchasePass.java
+++ src/ie/hotelbooking/view/PurchasePass.java
@@ -1,4 +1,12 @@
 package ie.hotelbooking.view;
 
-public class PurchasePass {
+import javax.swing.*;
+
+public class PurchasePass extends JPanel {
+	private final JLabel label1;
+
+	public PurchasePass() {
+		label1 = new JLabel("Purchase Pass");
+		add(label1);
+	}
 }
