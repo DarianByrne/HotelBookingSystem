d54628054ddae637b473af87392b28ca3ff0ac3d
kylepunta <kylepurcell1998@gmail.com>
2025-03-13 09:11:09 +0000

    Made changes to RoomBooking

--- src/ie/hotelbooking/model/booking/RoomBooking.java
+++ src/ie/hotelbooking/model/booking/RoomBooking.java
@@ -2,30 +2,11 @@ package ie.hotelbooking.model.booking;
 
 import ie.hotelbooking.model.location.*;
 
-/**
- *
- */
 public class RoomBooking extends Booking {
-
-	/**
-	 * Default constructor
-	 */
-	public RoomBooking() {
-	}
-
-	/**
-	 *
-	 */
 	private Room room;
-
-	/**
-	 *
-	 */
 	private int duration;
-
-	/**
-	 *
-	 */
 	private float rate;
 
+	public RoomBooking() {
+	}
 }
