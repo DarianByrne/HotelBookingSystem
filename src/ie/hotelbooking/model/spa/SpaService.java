7eba3cade9d1288ecf58e25dbce2759f4002f671
kylepunta <kylepurcell1998@gmail.com>
2025-03-31 11:50:05 +0100

    Added constructor, getter and setter methods to SpaService. Added instance variables and created a static variable for the class instance ID

--- src/ie/hotelbooking/model/spa/SpaService.java
+++ src/ie/hotelbooking/model/spa/SpaService.java
@@ -1,24 +1,33 @@
 package ie.hotelbooking.model.spa;
 
-/**
- *
- */
+
 public class SpaService {
-
-	/**
-	 * Default constructor
-	 */
-	public SpaService() {
-	}
-
-	/**
-	 *
-	 */
+	private int spaServiceID;
+	private static int spaServiceCounter = 0;
 	private String name;
-
-	/**
-	 *
-	 */
 	private float rate;
 
+	public SpaService() {}
+	public SpaService(String name, float rate) {
+		this.name = name;
+		this.rate = rate;
+	}
+	public int getSpaServiceID() {
+		return spaServiceID;
+	}
+	public void setSpaServiceID() {
+		spaServiceID = spaServiceCounter++;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getRate() {
+		return rate;
+	}
+	public void setRate(float rate) {
+		this.rate = rate;
+	}
 }
