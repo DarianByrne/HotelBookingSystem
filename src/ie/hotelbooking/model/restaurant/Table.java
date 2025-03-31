eeb780982db0c6698519c8d85aa1fc06b65c8e44
kylepunta <kylepurcell1998@gmail.com>
2025-03-31 11:50:41 +0100

    Added instance variable, static variable for table number, constructor, getter and setter methods to Table

--- src/ie/hotelbooking/model/restaurant/Table.java
+++ src/ie/hotelbooking/model/restaurant/Table.java
@@ -1,19 +1,18 @@
 package ie.hotelbooking.model.restaurant;
 
-/**
- *
- */
-public class Table {
 
-	/**
-	 * Default constructor
-	 */
+public class Table {
+	private int tableNumber;
+	private static int tableCounter = 0;
 	public Table() {
+		setTableNumber();
 	}
 
-	/**
-	 *
-	 */
-	private int numberOfSeats;
-
+	public int getTableNumber() {
+		return tableNumber;
+	}
+	public void setTableNumber() {
+		tableCounter++;
+		this.tableNumber = tableCounter;
+	}
 }
