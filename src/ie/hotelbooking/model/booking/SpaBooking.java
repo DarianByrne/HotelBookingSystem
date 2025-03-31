88cafd5aaae561d68f7efad811803f7249ed35b3
kylepunta <kylepurcell1998@gmail.com>
2025-03-31 11:49:15 +0100

    Added constructor, getter and setter methods to SpaBooking. Added instance variables and CRUD methods

--- src/ie/hotelbooking/model/booking/SpaBooking.java
+++ src/ie/hotelbooking/model/booking/SpaBooking.java
@@ -1,31 +1,79 @@
 package ie.hotelbooking.model.booking;
 
+import ie.hotelbooking.model.Database;
 import ie.hotelbooking.model.spa.SpaService;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
-/**
- *
- */
+
 public class SpaBooking extends Booking {
+	private SpaService spaService;
+	private int numberOfGuests;
 
-	/**
-	 * Default constructor
-	 */
 	public SpaBooking() {
+		super();
+	}
+	public SpaBooking(SpaService spaService, Date arrivalDate, Time arrivalTime, Time departureTime, int numberOfGuests) {
+		super();
+		this.spaService = spaService;
+		this.setArrivalDate(arrivalDate);
+		this.setArrivalTime(arrivalTime);
+		this.setDepartureTime(departureTime);
+		this.numberOfGuests = numberOfGuests;
+	}
+	public SpaService getSpaService() {
+		return spaService;
+	}
+	public void setSpaService(SpaService spaService) {
+		this.spaService = spaService;
+	}
+	public int getNumberOfGuests() {
+		return numberOfGuests;
+	}
+	public void setNumberOfGuests(int numberOfGuests) {
+		this.numberOfGuests = numberOfGuests;
 	}
 
-	/**
-	 *
-	 */
-	private SpaService spaService;
+	public void addSpaBooking() {
+		this.addBooking();
+		Connection con = null;
+		PreparedStatement ps = null;
+		int i = 0;
 
-	/**
-	 *
-	 */
-	private float duration;
-
-	/**
-	 *
-	 */
-	private float rate;
-
+		try {
+			con = Database.getConnection();
+			ps = con.prepareStatement("INSERT INTO spa_booking (date, arrivalTime, departureTime, numberOfGuests, spaServiceID) VALUES (?, ?, ?, ?, ?)");
+			ps.setDate(1, this.getArrivalDate());
+			ps.setTime(2, this.getArrivalTime());
+			ps.setTime(3, this.getDepartureTime());
+			ps.setInt(4, this.getNumberOfGuests());
+			ps.setInt(5, getSpaService().getSpaServiceID());
+			i = ps.executeUpdate();
+			System.out.println(i + " rows inserted");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+				ps.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public void updateSpaBooking() {
+		this.updateBooking();
+		Connection con = null;
+		PreparedStatement ps = null;
+		int i = 0;
+	}
+	public void deleteSpaBooking() {
+		this.deleteBooking();
+		Connection con = null;
+		PreparedStatement ps = null;
+		int i = 0;
+	}
 }
